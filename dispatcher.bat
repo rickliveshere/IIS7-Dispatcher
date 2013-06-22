06ecda3ace2d4e68806d6fa9d275706ad9a52f44
Richard Read <rickliveshere@yahoo.com>
2013-06-22 22:20:51 +0100

    Refactored dispatcher code
    Created functions - still need to add deployment and security functions

--- dispatcher.bat
+++ dispatcher.bat
@@ -16,53 +16,173 @@ set BindingProtocol=http
 set BindingIP=*
 set BindingPort=80
 set BindingHostHeader=www.contoso.com
+
+rem ------- BACKUP/ARCHIVING SETTINGS -------
 set BackupDirectory=C:\Backup
 
-echo "Creating app pool...."
-call appcmd add apppool /name:%AppPoolName% /managedRuntimeVersion:%DotNetVersion% /managedPipelineMode:"%PipelineMode%"
-if ERRORLEVEL 0 echo "App pool already setup!"
-
-echo "Creating website...."
-call APPCMD add site /name:%WebsiteName% /physicalPath:"%HostingDirectory%"
-if ERRORLEVEL 0 echo "Website already setup!"
-
-echo "Setting bindings...."
-call APPCMD set site /site.name:%WebsiteName% /+bindings.[protocol='%BindingProtocol%',bindingInformation='%BindingIP%:%BindingPort%:%BindingHostHeader%']
-
-echo "Assign website to app pool...."
-call APPCMD set app "%WebsiteName%/" /applicationpool:%AppPoolName%
-
-echo "Stopping website...."
-call APPCMD stop site "%WebsiteName%"
-
-IF EXIST C:\Backup GOTO BACKUPFOLDERCREATED
- 	MKDIR "%BackupDirectory%"
- 	echo "Backup directory created"
-:BACKUPFOLDERCREATED
-
-IF EXIST %HostingDirectory% GOTO READYFORBACKUP
-	echo "Creating hosting directory %HostingDirectory%"
-	MKDIR "%HostingDirectory%"
- 	echo "Hosting directory created"
-:READYFORBACKUP
-
-echo "Backing up existing artifacts...."
+rem --* The following code below will ensure your artifacts are backed up in a zip file labeled as [WebsiteName]-[Date]-[Time].zip *--
 rem cut off fractional seconds
 set t=%time:~0,8%
-rem replace colons with dashes
+rem remove colons
 set t=%t::=%
+rem remove slashes
 set d=%date:/=%
 set ArchiveName=%WebsiteName%-%d%-%t%
 
+rem ------- START OF BATCH SCRIPT -------
+
+rem ------- SCRIPT EXECUTION -------
+call :createAppPool
+call :setPipelineAndRuntime
+call :createWebsite
+
+call :setHostDirectory
+call :setBinding
+call :setAppPool
+call :createHostDirectory
+call :createBackupDirectory
+call :stopWebsite
+call :backup
+call :deploy
+call :startWebsite
+call :recycleAppPool
+call :end
+goto:eof
+rem ------- END OF SCRIPT EXECUTION -------
+
+rem ------- FUNCTIONS -------
+:createAppPool
+SETLOCAL
+echo Creating app pool....
+call appcmd add apppool /name:%AppPoolName%
+rem error code 183 returned for duplication
+if %ERRORLEVEL% GTR 0 if %ERRORLEVEL% NEQ 183 goto :setError %ERRORLEVEL%
+if %ERRORLEVEL% GTR 0 if %ERRORLEVEL% EQU 183 echo App pool %AppPoolName% already setup
+if %ERRORLEVEL% EQU 0 echo App pool %AppPoolName% created
+goto:eof
+ENDLOCAL
+
+:setPipelineAndRuntime
+SETLOCAL
+echo Setting managed runtime and pipeline mode....
+call appcmd set apppool /apppool.name:%AppPoolName% /managedRuntimeVersion:%DotNetVersion% /managedPipelineMode:"%PipelineMode%"
+if %ERRORLEVEL% GTR 0 goto :setError %ERRORLEVEL%
+echo Managed runtime set to %DotNetVersion%
+echo Pipeline mode set to %PipelineMode%
+goto:eof
+ENDLOCAL
+
+:createWebsite
+SETLOCAL
+echo Creating website....
+call APPCMD add site /name:%WebsiteName% /physicalPath:"%HostingDirectory%"
+if %ERRORLEVEL% GTR 0 if %ERRORLEVEL% NEQ 183 goto :setError %ERRORLEVEL%
+if %ERRORLEVEL% GTR 0 if %ERRORLEVEL% EQU 183 echo Website %WebsiteName% already setup!
+if %ERRORLEVEL% EQU 0 echo Website %WebsiteName% created
+goto:eof
+ENDLOCAL
+
+:setHostDirectory
+SETLOCAL
+echo Setting host directory....
+call APPCMD set vdir "%WebsiteName%/" -physicalPath:"%HostingDirectory%"
+if %ERRORLEVEL% GTR 0 goto :setError %ERRORLEVEL%
+echo Host directory set to %HostingDirectory%
+goto:eof
+ENDLOCAL
+
+:setBinding
+SETLOCAL
+echo Setting bindings....
+call APPCMD set site /site.name:%WebsiteName% /+bindings.[protocol='%BindingProtocol%',bindingInformation='%BindingIP%:%BindingPort%:%BindingHostHeader%']
+if %ERRORLEVEL% GTR 0 if %ERRORLEVEL% NEQ 183 goto :setError %ERRORLEVEL%
+if %ERRORLEVEL% GTR 0 if %ERRORLEVEL% EQU 183 echo Binding already setup!
+echo Binding set to %BindingProtocol%://%BindingHostHeader% - port %BindingPort% (ip %BindingIP%)
+goto:eof
+ENDLOCAL
+
+:setAppPool
+SETLOCAL
+echo Assign website to app pool....
+call APPCMD set app "%WebsiteName%/" /applicationpool:%AppPoolName%
+if %ERRORLEVEL% GTR 0 goto :setError %ERRORLEVEL%
+echo %WebsiteName% assigned to %AppPoolName%
+goto:eof
+ENDLOCAL
+
+:stopWebsite
+SETLOCAL
+echo Stopping website....
+call APPCMD stop site "%WebsiteName%"
+if %ERRORLEVEL% GTR 0 goto :setError %ERRORLEVEL%
+goto:eof
+ENDLOCAL
+
+:createBackupDirectory
+SETLOCAL
+IF EXIST %BackupDirectory% GOTO BACKUPFOLDERCREATED
+ 	MKDIR "%BackupDirectory%"
+ 	echo Backup directory created
+:BACKUPFOLDERCREATED
+if %ERRORLEVEL% GTR 0 goto :setError %ERRORLEVEL%
+goto:eof
+ENDLOCAL
+
+:createHostDirectory
+SETLOCAL
+IF EXIST %HostingDirectory% GOTO READYFORBACKUP
+	echo Creating hosting directory %HostingDirectory%
+	MKDIR "%HostingDirectory%"
+ 	echo Hosting directory created
+:READYFORBACKUP
+if %ERRORLEVEL% GTR 0 goto :setError %ERRORLEVEL%
+goto:eof
+ENDLOCAL
+
+:backup
+SETLOCAL
+echo Backing up existing artifacts....
 call 7z a -tzip "%BackupDirectory%\%ArchiveName%.zip" "%HostingDirectory%\*.*" -mx5
+if %ERRORLEVEL% GTR 0 goto :setError %ERRORLEVEL%
+echo Backup created - %BackupDirectory%\%ArchiveName%.zip
+goto:eof
+ENDLOCAL
 
-echo "Deploying artifacts...."
+:deploy
+SETLOCAL
+echo Deploying artifacts....
 rem todo
-
-echo "Deployed"
+echo Deployed
 rem todo
+goto:eof
+ENDLOCAL
 
-echo "Starting website...."
+:startWebsite
+SETLOCAL
+echo Starting website....
 call APPCMD start site "%WebsiteName%"
+goto:eof
+ENDLOCAL
 
-echo "Website started"
+:recycleAppPool
+SETLOCAL
+echo Recycling app pool....
+call APPCMD recycle apppool /apppool.name:%AppPoolName%
+goto:eof
+ENDLOCAL
+
+:setError
+SETLOCAL
+if %ERRORLEVEL% GTR 0 echo There has been a problem dispatching the website. See the command log for details
+if %ERRORLEVEL% GTR 0 Exit /B %1
+goto:eof
+ENDLOCAL
+
+:end
+SETLOCAL
+echo End of dispatch
+goto:eof
+ENDLOCAL
+rem ------- END OF FUNCTIONS -------
+
+rem ------- END OF BATCH SCRIPT -------
